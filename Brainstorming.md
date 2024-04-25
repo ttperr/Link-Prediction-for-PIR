a10213b5ee91521350ac59b4bf75a05afc5ee593
mazerti <titouan.mazier@grenoble-inp.org>
2024-04-25 17:06:49 +0200

    get started with reranking

---
+++ Brainstorming.md
@@ -0,0 +1,20 @@
+
+- [ ] build dataset -> Tristan
+- [ ] index dataset for elasticsearch -> Tristan
+- [ ] try queries -> Tristan
+- [ ] parse the logs -> Titouan
+- [ ] compute ranks -> Hani
+- [ ] user interface -> Lorenzo
+- [ ] Start report -> Lorenzo
+
+- [ ] elastic search
+	- just do it
+- [ ] personalization re-ranking
+	- simple heuristics (bad but easy)
+		- P-Click
+		- L-Profile
+		- S-Profile
+		- LS-Profile
+		- G-Click
+	- graph based
+	- contextual features
--- Reranker.py
+++ Reranker.py
@@ -1,21 +1,22 @@
+import csv
 import numpy as np
+from tqdm import tqdm
+from util import parseCsv
 
 class Reranker(object):
+
     def __init__(self,dataset):
         if dataset=="AOL":
             #YOU SHOULD LOAD VARIABLES/USEFUL INFO FROM LOGS OR A FILE STORING THEM (IF YOU USE FILES, STORE THEM IN THE CORRECT FOLDER)
             #THE FOLDER with data and files needed to rerank SHOULD BE in  AOL4PS/ 
-            pass
+            self.dataset = dataset
         else:
             raise NotImplementedError("Unknown dataset")
 
     def rerank(self,query_text,query_results,user):
         #JUST A RANDOM FUNCTION TO SEE DIFFERENT RESULTS(RANDOM)
-        res=[]
-        for doc,score in query_results:
-            res.append((doc,score*np.random.rand()))
-        #NO NEED TO SORT THEM AT THE END, I'LL DO THAT IN THE GUI 
-        return res
+        queryID = self.getQueryID(query_text)
+        return self.PClick(queryID, query_results, user)
 
     def is_new_user(self,userID):
         #Must return true if the userId is new, false if it is known
@@ -24,3 +25,51 @@ class Reranker(object):
         if np.random.rand()>0.5:
             return True
         return False
+    
+    def getQueryID(self, query_text):
+        """parse query.csv and return matching query id"""
+        maxId = 0
+        if self.dataset=="AOL":
+            with open('datasets/AOL4PS/query.csv') as f:
+                reader = csv.reader(f, delimiter='\t')
+                firstRow = True
+                pbar = tqdm(reader, desc='Parsing queries', unit='rows')
+                for row in pbar:
+                    if firstRow:
+                        firstRow = False
+                        continue
+                    if row[0] == query_text:
+                        return row[1]
+                    idNumber = int(row[1][2:])
+                    if idNumber > maxId:
+                        maxId = idNumber
+        else:
+            raise NotImplementedError("Unknown dataset")
+        return f"q-{maxId+1}"
+
+
+    def PClick(self, queryID, query_results, userID):
+        """Hello world metric, defined in https://dl.acm.org/doi/10.1145/1242572.1242651"""
+        ranking_ratio = 0.8
+        potentialMatches = 0
+        matches = {}
+        beta = 0.5
+        res=[]
+        if self.dataset=="AOL":
+            with open('datasets/AOL4PS/data.csv') as f:
+                reader = csv.reader(f, delimiter='\t')
+                firstRow = True
+                pbar = tqdm(reader, desc='Parsing logs', unit='rows')
+                for row in pbar:
+                    if firstRow:
+                        firstRow = False
+                        continue
+                    if row[1] == queryID and row[0] == userID:
+                        potentialMatches += 1
+                        matches[row[5]] =  (matches[row[5]] + 1 if row[5] in matches else 1)
+        else:
+            raise NotImplementedError("Unknown dataset")
+        for resultID ,score in query_results:
+            newScore = score * (1 - ranking_ratio) + (matches[resultID]/(potentialMatches+beta) if resultID in matches else 0) * ranking_ratio
+            res.append((resultID, newScore))
+        return res
