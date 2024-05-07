17442b89dec028de1cd838845e66598d982a6170
mazerti <titouan.mazier@grenoble-inp.org>
2024-05-07 10:20:21 +0200

    Cleaning comments on graph.py and reranker.py

--- Reranker.py
+++ Reranker.py
@@ -4,47 +4,50 @@ import numpy as np
 from tqdm import tqdm
 from graph import Graph
 
+
 class Reranker(object):
 
-    def __init__(self,dataset,validation=False):
+    def __init__(self, dataset, validation=False):
         self.dataset = dataset
-        self.validation=validation
+        self.validation = validation
         self.ranking_ratio = 1
-        if dataset=="AOL":
-            #YOU SHOULD LOAD VARIABLES/USEFUL INFO FROM LOGS OR A FILE STORING THEM (IF YOU USE FILES, STORE THEM IN THE CORRECT FOLDER)
-            #THE FOLDER with data and files needed to rerank SHOULD BE in  AOL4PS/ 
-            self.user_document = Graph(0, 5, self.validation, dataset=self.dataset, name="user-document")
+        if dataset == "AOL":
+            self.user_document = Graph(
+                0, 5, self.validation, dataset=self.dataset, name="user-document"
+            )
             self.UD_metrics = {}
+            # ---- The following lines would load the query-document and query-user graphs. This is meant for further developments.
             # self.query_document = Graph(1, 5, self.validation, dataset=self.dataset, name="query-document")
             # self.query_user= Graph(1, 0, self.validation, dataset=self.dataset, name="query-user")
-            self.METRICS = {
-                "user_document-shortest_distance": self.user_document.shortest_distance,
-                "user_document-weighted_shortest_distance": self.user_document.weighted_shortest_distance,
-                "user_document-common_neighbors": self.user_document.common_neighbors_metric,
+
+            self.METRICS = {  # ---- Lists the metrics computed to run the reranker. Uncomment more lines if you want to test more.
+                # "user_document-shortest_distance": self.user_document.shortest_distance,
+                # "user_document-weighted_shortest_distance": self.user_document.weighted_shortest_distance,
+                # "user_document-common_neighbors": self.user_document.common_neighbors_metric,
                 "user_document-adamic_adar": self.user_document.adamic_adar_metric,
-                "user_document-page_rank": self.user_document.rooted_page_rank,
+                # "user_document-page_rank": self.user_document.rooted_page_rank,
                 "user_document-prop_flow": self.user_document.prop_flow,
             }
         else:
             raise NotImplementedError("Unknown dataset")
-    def updateGraphFromClicks(self,user_id,session,query_id,doc_id):
-        '''
+
+    def updateGraphFromClicks(self, user_id, session, query_id, doc_id):
+        """
         Updates the graphs from user clicks.
         Args:
             user_id: a string representing the user id. Possibly new.
             session: a string representing the session. It is relative to the user id.
             query_id: a string representing the query id. Possibly new.
             doc_id: a string representing the clicked document. Possibly new (wrt the graph, not the collection).
-        '''
+        """
         self.user_document.add_link(user_id, doc_id)
-        # self.session_document.add_link(session, doc_id)
+        # ---- The following lines are meant to be uncommented for implementations using query-document/query-user graphs.
         # self.query_document.add_link(query_id, doc_id)
         # self.query_user.add_link(query_id, user_id)
-        # self.query_session.add_link(query_id, session)
 
-    def rerank(self,query_id,user,retrieved_docs,retrieved_scores,session=None):
-        '''
-        Computes the reranking scores based on various arguments. 
+    def rerank(self, query_id, user, retrieved_docs, retrieved_scores, session=None):
+        """
+        Computes the reranking scores based on various arguments.
 
         Args:
             query_id: a string containing the query id.
@@ -54,94 +57,110 @@ class Reranker(object):
             session: a string containing the session id. Must support None, i.e. when the sesion is not provided.
         Returns:
             reranked_scores: a np.array of the same shape and type as retrieved_scores, with updated values.
-        '''
+        """
         if len(retrieved_docs) == 0:
             return None
 
         self.compute_UD_metrics(user, retrieved_docs)
-        return self.mix_scores(
+        return self.mix_scores(  # The following lines lists the metrics used and their respective weights in the final ranking. Make sure to comment all metrics that does not appear in self.METRIC self.METRIC.
             (retrieved_scores, 1),
-            (self.UD_metrics["user_document-shortest_distance"], 1),
-            (self.UD_metrics["user_document-weighted_shortest_distance"], 1),
-            (self.UD_metrics["user_document-common_neighbors"], 1),
-            (self.UD_metrics["user_document-adamic_adar"], 1),
-            (self.UD_metrics["user_document-page_rank"], 1),
-            (self.UD_metrics["user_document-prop_flow"], 1)
+            # (self.UD_metrics["user_document-shortest_distance"], 1),
+            # (self.UD_metrics["user_document-weighted_shortest_distance"], 1),
+            # (self.UD_metrics["user_document-common_neighbors"], 1),
+            (self.UD_metrics["user_document-adamic_adar"], 2),
+            # (self.UD_metrics["user_document-page_rank"], 1),
+            (self.UD_metrics["user_document-prop_flow"], 1),
         )
-    
+
     def compute_UD_metrics(self, user, retrieved_docs, progress=False, metrics="all"):
         time_start = time.time()
         if metrics == "all":
             metrics = list(self.METRICS.keys())
 
-        if progress: print("Computing user-document metrics")
+        if progress:
+            print("Computing user-document metrics")
         for metric in metrics:
-            if progress: print(metric + "...", end="")
+            if progress:
+                print(metric + "...", end="")
             self.UD_metrics[metric] = self.METRICS.get(metric)(user, retrieved_docs)
-            if progress: print("done")
-        if progress: print(f"Computations took {time.time()-time_start}s")
+            if progress:
+                print("done")
+        if progress:
+            print(f"Computations took {time.time()-time_start}s")
 
-    def evaluation_metrics_scores(self,query_id,user,retrieved_docs,retrieved_scores,session=None, metrics="all"):
-        '''
-        Computes reranking scores based on all metrics chosen for evaluation.
+    def evaluation_metrics_scores(
+        self,
+        query_id,
+        user,
+        retrieved_docs,
+        retrieved_scores,
+        session=None,
+        metrics="all",
+    ):
+        """
+        Computes re-ranking scores based on all metrics chosen for evaluation.
 
         Args:
             query_id: a string containing the query id.
             user: a string containing the user id. Can be new.
             retrieved_docs: a np.array of strings, representing the doc ids.
             retrieved_scores: a np.array of floats, containing the scores given by ElasticSearch. Index is the same as the docs. May be unsorted.
-            session: a string containing the session id. Must support None, i.e. when the sesion is not provided.
+            session: a string containing the session id. Must support None, i.e. when the session is not provided.
             metrics: a np.array containing the unique name of the metrics.
         Returns:
-            reranked_scores: a np.array of the shape n_docs x metrics, containing the score for each doc. Must have the same order as the rows of metrics. 
-        '''
+            re-ranked_scores: a np.array of the shape n_docs x metrics, containing the score for each doc. Must have the same order as the rows of metrics.
+        """
         if len(retrieved_docs) == 0:
             return None
         if metrics == "all":
             metrics = list(self.METRICS.keys())
         self.compute_UD_metrics(user, retrieved_docs, metrics=metrics)
         return np.transpose(np.matrix([self.UD_metrics[metric] for metric in metrics]))
-        
 
-    def is_new_user(self,userID): #TODO
-        #Must return true if the userId is new, false if it is known
+    def is_new_user(self, userID):
+        """
+        Must return true if the userId is new, false if it is known
+        """
         return not self.user_document.nx_graph.has_node(userID)
-
 
     def PClick(self, queryID, retrieved_docs, retrieved_scores, userID):
         """Hello world metric, defined in https://dl.acm.org/doi/10.1145/1242572.1242651"""
         potentialMatches = 0
         matches = {}
         beta = 0.5
-        res=[]
-        if self.dataset=="AOL":
-            with open('datasets/AOL4PS/data.csv') as f:
-                reader = csv.reader(f, delimiter='\t')
+        res = []
+        if self.dataset == "AOL":
+            with open("datasets/AOL4PS/data.csv") as f:
+                reader = csv.reader(f, delimiter="\t")
                 firstRow = True
-                pbar = tqdm(reader, desc='Parsing logs', unit='rows')
+                pbar = tqdm(reader, desc="Parsing logs", unit="rows")
                 for row in pbar:
                     if firstRow:
                         firstRow = False
                         continue
                     if row[1] == queryID and row[0] == userID:
                         potentialMatches += 1
-                        matches[row[5]] =  (matches.get(row[5], 0) + 1)
+                        matches[row[5]] = matches.get(row[5], 0) + 1
         else:
             raise NotImplementedError("Unknown dataset")
-        for resultID ,score in zip(retrieved_docs, retrieved_scores):
-            newScore = score * (1 - self.ranking_ratio) + (matches.get(resultID, 0)/(potentialMatches+beta)) * self.ranking_ratio
+        for resultID, score in zip(retrieved_docs, retrieved_scores):
+            newScore = (
+                score * (1 - self.ranking_ratio)
+                + (matches.get(resultID, 0) / (potentialMatches + beta))
+                * self.ranking_ratio
+            )
             res.append(newScore)
         return np.array(res)
-    
+
     def mix_scores(self, *arg):
-        '''
-        Mix reranking scores.
+        """
+        Mix re-ranking scores.
 
         Args:
             a list of (score, weight) where weight is an float and score is a np.array of shape n_docs x 1
         Returns:
-            reranked_scores: a np.array of the shape n_docs x 1, containing the mixed score for each doc.
-        '''
+            re-ranked_scores: a np.array of the shape n_docs x 1, containing the mixed score for each doc.
+        """
         if len(arg) == 0:
             raise ValueError("No metric given.")
         shape = arg[0][0].shape
@@ -149,63 +168,69 @@ class Reranker(object):
         for score, weight in arg:
             assert score.shape == shape
             norm = np.linalg.norm(score)
-            normalized_score = score / norm if norm != 0 else np.zeros(shape=score.shape)
+            normalized_score = (
+                score / norm if norm != 0 else np.zeros(shape=score.shape)
+            )
             res += weight * normalized_score
         return res
-    
+
     def graph_features(self, query, user, documents, session=None):
+        """
+        Generate features for a given query to feed ML algorithm (unimplemented).
+        """
         tmp = self.ranking_ratio
         self.ranking_ratio = 1
         k = len(documents)
-        res = np.matrix([
-            # nodes degrees
-            [self.user_document.degree(user)] * k,
-            [self.user_document.degree(document) for document in documents],
-            [self.session_document.degree(session)] * k,
-            [self.session_document.degree(document) for document in documents],
-            [self.query_document.degree(query)] * k,
-            [self.query_document.degree(document) for document in documents],
-            [self.query_user.degree(query)] * k,
-            [self.query_user.degree(user)] * k,
-            [self.query_session.degree(query)] * k,
-            [self.query_session.degree(session)] * k,
-            # shortest distance
-            self.UD_shortest_distance,
-            self.SD_shortest_distance,
-            self.QD_shortest_distance,
-            [self.QU_shortest_distance] * k,
-            [self.QS_shortest_distance] * k,
-            # weighted shortest distance (probably don't need both shortest distance metrics)
-            self.UD_weighted_shortest_distance,
-            self.SD_weighted_shortest_distance,
-            self.QD_weighted_shortest_distance,
-            [self.QU_weighted_shortest_distance] * k,
-            [self.QS_weighted_shortest_distance] * k,
-           # Common neighbors
-            self.UD_common_neighbors,
-            self.SD_common_neighbors,
-            self.QD_common_neighbors,
-            [self.QU_common_neighbors] * k,
-            [self.QS_common_neighbors] * k,
-            # Adamic Adar
-            self.UD_adamic_adar,
-            self.SD_adamic_adar,
-            self.QD_adamic_adar,
-            [self.QU_adamic_adar] * k,
-            [self.QS_adamic_adar] * k,
-            # page rank
-            self.UD_page_rank,
-            self.SD_page_rank,
-            self.QD_page_rank,
-            [self.QU_page_rank] * k,
-            [self.QS_page_rank] * k,
-            # prop flow
-            self.UD_prop_flow,
-            self.SD_prop_flow,
-            self.QD_prop_flow,
-            [self.QU_prop_flow] * k,
-            [self.QS_prop_flow] * k,
-        ])
+        res = np.matrix(
+            [
+                # nodes degrees
+                [self.user_document.degree(user)] * k,
+                [self.user_document.degree(document) for document in documents],
+                [self.session_document.degree(session)] * k,
+                [self.session_document.degree(document) for document in documents],
+                [self.query_document.degree(query)] * k,
+                [self.query_document.degree(document) for document in documents],
+                [self.query_user.degree(query)] * k,
+                [self.query_user.degree(user)] * k,
+                [self.query_session.degree(query)] * k,
+                [self.query_session.degree(session)] * k,
+                # shortest distance
+                self.UD_shortest_distance,
+                self.SD_shortest_distance,
+                self.QD_shortest_distance,
+                [self.QU_shortest_distance] * k,
+                [self.QS_shortest_distance] * k,
+                # weighted shortest distance (probably don't need both shortest distance metrics)
+                self.UD_weighted_shortest_distance,
+                self.SD_weighted_shortest_distance,
+                self.QD_weighted_shortest_distance,
+                [self.QU_weighted_shortest_distance] * k,
+                [self.QS_weighted_shortest_distance] * k,
+                # Common neighbors
+                self.UD_common_neighbors,
+                self.SD_common_neighbors,
+                self.QD_common_neighbors,
+                [self.QU_common_neighbors] * k,
+                [self.QS_common_neighbors] * k,
+                # Adamic Adar
+                self.UD_adamic_adar,
+                self.SD_adamic_adar,
+                self.QD_adamic_adar,
+                [self.QU_adamic_adar] * k,
+                [self.QS_adamic_adar] * k,
+                # page rank
+                self.UD_page_rank,
+                self.SD_page_rank,
+                self.QD_page_rank,
+                [self.QU_page_rank] * k,
+                [self.QS_page_rank] * k,
+                # prop flow
+                self.UD_prop_flow,
+                self.SD_prop_flow,
+                self.QD_prop_flow,
+                [self.QU_prop_flow] * k,
+                [self.QS_prop_flow] * k,
+            ]
+        )
         self.ranking_ratio = tmp
         return res
-    
--- graph.py
+++ graph.py
@@ -11,8 +11,9 @@ from tqdm import tqdm
 
 EPSILON = 0.001
 
+
 class Graph(object):
-    
+
     def __init__(self, index1, index2, validation, dataset="AOL", name=None) -> None:
         """initialize the graph, empty"""
         self.name = str(datetime.timestamp) if name == None else name
@@ -21,19 +22,19 @@ class Graph(object):
 
     def add_link(self, node1, node2):
         if self.nx_graph.has_edge(node1, node2):
-            self.nx_graph[node1][node2]['weight'] += 1
+            self.nx_graph[node1][node2]["weight"] += 1
         else:
             self.nx_graph.add_edge(node1, node2, weight=1)
-    
+
     def populate_graph_from_logs(self, index1, index2, validation, dataset="AOL"):
-        if dataset=="AOL":
-            doc='datasets/AOL4PS/data.csv'
+        if dataset == "AOL":
+            doc = "datasets/AOL4PS/data.csv"
             if validation:
-                doc='datasets/AOL4PS/training_data.csv'
+                doc = "datasets/AOL4PS/training_data.csv"
             with open(doc) as f:
-                reader = csv.reader(f, delimiter='\t')
+                reader = csv.reader(f, delimiter="\t")
                 firstRow = True
-                pbar = tqdm(reader, desc='Parsing queries', unit='rows')
+                pbar = tqdm(reader, desc="Parsing queries", unit="rows")
                 for row in pbar:
                     if firstRow:
                         firstRow = False
@@ -41,12 +42,12 @@ class Graph(object):
                     self.add_link(row[index1], row[index2])
         else:
             raise NotImplementedError("Unknown dataset")
-    
+
     def get_weight(self, node1, node2):
         if self.nx_graph.has_edge(node1, node2):
-            return self.nx_graph[node1][node2]['weight']
+            return self.nx_graph[node1][node2]["weight"]
         return 0
-    
+
     def neighbors(self, node) -> set:
         return set(self.nx_graph.neighbors(node))
 
@@ -56,18 +57,27 @@ class Graph(object):
     def shortest_distance(self, start, targets):
         if not self.nx_graph.has_node(start):
             return np.zeros(shape=(len(targets)))
-        lengths = nx.shortest_path_length(self.nx_graph, start, weight=lambda u,v,e: 1/e.get('weight', 0))
+        lengths = nx.shortest_path_length(
+            self.nx_graph, start, weight=lambda u, v, e: 1 / e.get("weight", 0)
+        )
         return np.array([lengths.get(arrival, 0) + EPSILON for arrival in targets])
 
     def weighted_shortest_distance(self, start, targets):
         """Each edge is weighted by the degree of arrival node. Going to a more popular node is less significant"""
         if not self.nx_graph.has_node(start):
             return np.zeros(shape=(len(targets)))
-        lengths = nx.shortest_path_length(self.nx_graph, start, weight=lambda u,v,e: self.nx_graph.degree(v)/e.get('weight', 0))
+        lengths = nx.shortest_path_length(
+            self.nx_graph,
+            start,
+            weight=lambda u, v, e: self.nx_graph.degree(v) / e.get("weight", 0),
+        )
         return np.array([lengths.get(arrival, 0) + EPSILON for arrival in targets])
 
     def common_neighbors_metric(self, node1, targets):
-        return np.array([self.common_neighbors_single(node1, node2) for node2 in targets])
+        """Wrapper for the common neighbors metric computation meant to be run on an array of target nodes."""
+        return np.array(
+            [self.common_neighbors_single(node1, node2) for node2 in targets]
+        )
 
     def common_neighbors_single(self, node1, node2):
         """
@@ -88,11 +98,13 @@ class Graph(object):
         return res
 
     def adamic_adar_metric(self, node1, targets):
+        """Wrapper for the Adamic-Adar metric computation meant to be run on an array of target nodes."""
         return np.array([self.adamic_adar_single(node1, node2) for node2 in targets])
 
     def adamic_adar_single(self, node1, node2):
         """
-        Adamic-Adar similarity metrics defined in https://www.sciencedirect.com/science/article/pii/S0378873303000091?via%3Dihub
+        Adamic-Adar similarity metrics defined in https://www.sciencedirect.com/science/article/pii/S0378873303000091?via%3Dihub.
+        It has been adapted to the bipartite aspect of the graph in the same way as common neighbors.
         """
         if not self.nx_graph.has_node(node1) or not self.nx_graph.has_node(node2):
             return 0
@@ -101,26 +113,34 @@ class Graph(object):
         for node in neighbors_node1:
             tmp = 0
             for z in nx.common_neighbors(self.nx_graph, node, node2):
-                tmp += 1/math.log(self.degree(z)+EPSILON)
+                tmp += 1 / math.log(self.degree(z) + EPSILON)
             res += tmp * self.get_weight(node1, node) / self.degree(node)
         neighbors_node2 = self.neighbors(node2)
         for node in neighbors_node2:
             tmp = 0
             for z in nx.common_neighbors(self.nx_graph, node, node1):
-                tmp += 1/math.log(self.degree(z)+EPSILON)
+                tmp += 1 / math.log(self.degree(z) + EPSILON)
             res += tmp * self.get_weight(node2, node) / self.degree(node)
         return res
-    
+
     def rooted_page_rank(self, root_node, target_nodes):
+        """Compute a page rank metric where all the jumps are made back to the root node."""
         if not self.nx_graph.has_node(root_node):
             return np.zeros(shape=(len(target_nodes)))
-        pagerank_scores = nx.pagerank(self.nx_graph, weight='weight', personalization={root_node: 1})
-        return np.array([pagerank_scores.get(node, 0) + EPSILON for node in target_nodes])
+        pagerank_scores = nx.pagerank(
+            self.nx_graph, weight="weight", personalization={root_node: 1}
+        )
+        return np.array(
+            [pagerank_scores.get(node, 0) + EPSILON for node in target_nodes]
+        )
 
-    def prop_flow(self, source_node, targets, max_length:int=5):
+    def prop_flow(self, source_node, targets, max_length: int = 5):
+        """
+        Prop flow similarity metric proposed by Lichtenwalter et al. in https://doi.org/10.1145/1835804.1835837.
+        """
         if not self.nx_graph.has_node(source_node):
             return np.zeros(shape=(len(targets)))
-        visit_probability = {source_node: 1.0}  
+        visit_probability = {source_node: 1.0}
         for step in range(max_length):
             next_visit_probability = {}
             for current_node, prob in visit_probability.items():
@@ -134,14 +154,15 @@ class Graph(object):
                     next_visit_probability[neighbor] += transfer_prob
 
             visit_probability = next_visit_probability
-        
+
         return np.array([visit_probability.get(node, 0) for node in targets])
 
     def display_stats(self):
-        '''
-        Dipslay the stats of the graph.
-        Please leave this function at the end of the file.
-        '''
+        """
+        Display information about the graph.
+
+        The code for the plots is adapted from networkx documentation: https://networkx.org/documentation/stable/auto_examples/drawing/plot_degree.html#sphx-glr-auto-examples-drawing-plot-degree-py
+        """
         G = self.nx_graph
         print(f"Graph {self.name}:")
         print(f"|N| = {len(G.nodes)}\t |E| = {len(G.edges)}")
@@ -149,17 +170,17 @@ class Graph(object):
         giant_component = sorted(list(nx.connected_components(G)))[0]
         print(f"giant component: {len(giant_component)}")
         giant_component_graph = G.subgraph(giant_component)
-        print(f"diameter(lower bound): {nx.algorithms.approximation.diameter(giant_component_graph)}")
+        print(
+            f"diameter(lower bound): {nx.algorithms.approximation.diameter(giant_component_graph)}"
+        )
         giant_component_subset = random.choices(list(giant_component), k=30)
         avg = 0
         for node in giant_component_subset:
-            avg+=np.average(list(nx.shortest_path_length(G, node).values()))/30
+            avg += np.average(list(nx.shortest_path_length(G, node).values())) / 30
         print(f"average distance: {avg}")
 
         degree_sequence = sorted((d for n, d in G.degree()), reverse=True)
-        dmax = max(degree_sequence)
         fig = plt.figure("Degree of a random graph", figsize=(8, 4))
-        # Create a gridspec for adding subplots of different sizes
         axgrid = fig.add_gridspec(2, 2)
 
         ax1 = fig.add_subplot(axgrid[:, :1])
@@ -172,7 +193,6 @@ class Graph(object):
 
         ax2 = fig.add_subplot(axgrid[:, 1:])
         ax2.hist(degree_sequence)
-        # ax2.set_yscale("log")
         ax2.set_title("Degree histogram")
         ax2.set_xlabel("Degree")
         ax2.set_ylabel("# of Nodes")
@@ -180,6 +200,7 @@ class Graph(object):
         fig.tight_layout()
         plt.show()
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     user_pages = Graph(0, 5, False, name="user-pages")
     user_pages.display_stats()
